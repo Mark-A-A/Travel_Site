41d489733a7fe823e62a6d8a3f411fd1cf9f1d9d
Mark-A-A <markawai01@gmail.com>
2015-12-01 18:29:14 -0500

    modified the JS

---
+++ js/hotel.js
@@ -0,0 +1,32 @@
+$(document).on('click', '.amenities.dropdown-menu li a', function() {
+    $('#amenities').val($(this).html());
+}); 
+
+$(document).on('click', '.price.dropdown-menu li a', function() {
+    $('#priceinput').val($(this).html());
+}); 
+
+$(document).on('click', '.rating.dropdown-menu li a', function() {
+    $('#ratinginput').val($(this).html());
+}); 
+
+
+$(document).ready(function(){
+  
+  
+  $("#search").on("click", function(e){
+    e.preventDefault();  
+    var city = $("#pick-a-city").val();
+    var price = $("#pick-a-price").val();
+    var rating = $("#rating-stars").val();
+    var amenities = [];
+
+    //If no city is selected; alert please specify a city
+
+    //When city is selected, if it is NYC(for example), then display all hotels in new york 
+    //When star rating is selected, it is "__blank__", then display 
+  });
+
+
+
+}); //End of Document Function
