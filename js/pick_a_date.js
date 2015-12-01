1068dfe16fb36185ca56e22f25f2822276df81c0
Mark-A-A <markawai01@gmail.com>
2015-12-01 13:47:47 -0500

    check in and check out date inputs, messing with calendar js

--- js/pick_a_date.js
+++ js/pick_a_date.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   //e.
-  $(".datepicker").pickadate();
+  //$(".datepicker").pickadate();
   //$('.timepicker').pickatime();
-/*
+
   $('.datepicker').pickadate({
     labelMonthNext: 'Go to the next month',
     labelMonthPrev: 'Go to the previous month',
@@ -10,8 +10,9 @@ $(document).ready(function(){
     labelYearSelect: 'Pick a year from the dropdown',
     selectMonths: true,
     selectYears: true,
+    //min: new Date(),
   })
-*/
+
 })  // end of document object
 
 
